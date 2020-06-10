ecc294242d9a723c535c5db034550f00ef33df28
Wesley Valdes <wesley.valdes@gmail.com>
2020-06-10 08:28:48 -0700

    ChatHub design and javascript

---
+++ SignalRCore.Mvc.Demo/SignalRCore.Chat.Mvc/Models/ChatUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SignalRCore.Chat.Mvc.Models
+{
+    public class ChatUser : IdentityUser
+    {
+        public  virtual ICollection<Message> Messages { get; set; }
+    }
+}
